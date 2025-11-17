23f2f547d01fb7569c46bfebf3108db86073c74c
Teragram04 <margaretparsons3304@gmail.com>
2025-11-17 15:21:04 -0600

    Test flask, deploy on development server

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# TO RUN, COMMAND IS python -m flask --app Test run
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
