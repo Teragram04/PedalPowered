552bb8b5a2769383b90fd92bcc8e7948c493e61c
Teragram04 <margaretparsons3304@gmail.com>
2025-11-17 22:02:48 -0600

    PED-8 Create hompage template, rname Test.py to PedalPowered

---
+++ PedalPowered.py
@@ -0,0 +1,8 @@
+# TO RUN, COMMAND IS python -m flask --app Test run
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
