562ac7704c96f529dffc251d26ba96c6e3291157
Teragram04 <margaretparsons3304@gmail.com>
2025-11-23 22:39:26 -0500

    PED-3 require login for certain pages, add logout button once logged in

--- pedalpowered/routes.py
+++ pedalpowered/routes.py
@@ -1,23 +1,28 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from pedalpowered.models import User, rides
 from pedalpowered.forms import RegisterForm, LoginForm
 from pedalpowered import app, db, bcrypt
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/home")
+@login_required
 def home():
     return render_template("home.html")
 
 @app.route("/logride")
+@login_required
 def logride():
     return render_template("logride.html", title="Log a Ride")
 
 @app.route("/stats")
+@login_required
 def stats():
     return render_template("stats.html", title="Your Stats")
 
 @app.route("/register", methods = ['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
         #Hash the password so it can't be taken directly off the database
@@ -33,16 +38,31 @@ def register():
 @app.route("/", methods = ['GET','POST'])
 @app.route("/login", methods = ['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
 
         user = User.query.filter_by(username = form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next')
             flash(f'Welcome back {form.username.data}!','success')
-            return redirect(url_for('home'))
+            #Ternary conditional!
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Login failed, please try again.','danger')
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Login")
+
 
 #python -m flask --app PedalPowered run
