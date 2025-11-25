9ac9d958a9f03fa8f56f398d168ed0d40533a7ff
Teragram04 <margaretparsons3304@gmail.com>
2025-11-24 22:19:40 -0500

    PED-1 add profile pic capability

--- pedalpowered/routes.py
+++ pedalpowered/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
 from pedalpowered.models import User, rides
-from pedalpowered.forms import RegisterForm, LoginForm
+from pedalpowered.forms import RegisterForm, LoginForm, UpdateAcctForm
 from pedalpowered import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 @app.route("/home")
 @login_required
@@ -59,10 +62,40 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/account")
+
+def set_profile_pic(form_picture):
+    random_pic_name = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_file_name = random_pic_name + f_ext
+    pic_path = os.path.join(app.root_path, 'static/profile_pics',picture_file_name)
+    output_size = (100,100)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(pic_path)
+    return  picture_file_name
+
+
+
+
+
+@app.route("/account", methods = ['GET','POST'])
 @login_required
 def account():
-    return render_template("account.html", title="Login")
+    form = UpdateAcctForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = set_profile_pic(form.picture.data)
+            current_user.img_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Account info upates', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.img_file)
+    return render_template("account.html", title="Login", image_file = image_file, form=form)
 
 
 #python -m flask --app PedalPowered run
